555e64bcfa1b7a9616a4cd88473e644fad01cdde
School <school@Ethans-MacBook-Pro-3.local>
2025-03-14 23:44:55 +1100

    fix: converted authentication to jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthenticationApp from './js/authentication.jsx';
+
+function App() {
+  return (
+    <div>
+      <AuthenticationApp />
+    </div>
+  )
+}
+
+export default App;
