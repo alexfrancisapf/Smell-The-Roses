3f8a8f08d04d89c3ae004232542f889d54a68304
Alex Francis <alexfrancis.apf@gmail.com>
2025-03-15 22:32:52 +1100

    Very basic login page

--- client/src/components/authentication.jsx
+++ client/src/components/authentication.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
+import { Typography } from '@mui/material';
 
 // * Firebase configuration
 const firebaseConfig = {
@@ -63,34 +66,53 @@ function AuthenticationApp() {
   };
 
   return (
-    <>
-      <form>
+    <div>
+      <form className="auth">
+      <Typography variant="h5" component="h3" gutterBottom>
+        Welcome to Smell the Roses
+      </Typography>
         <div className="form-group">
-          <input
-            type="email" className="form-control"
+
+          <TextField
+            hiddenLabel
             id="email-input"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
+            variant="outlined"
+            size="small"
+            placeholder="Email"
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: '25px', // Circular edges
+              },
+            }}
           />
-        </div>
+          </div>
         <div className="form-group">
-          <input
-            type="password"
-            className="form-control"
+          <TextField
+            hiddenLabel
             id="password-input"
+            variant="outlined"
+            size="small"
             placeholder="Password"
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            fullWidth
+            sx={{
+              '& .MuiOutlinedInput-root': {
+                borderRadius: '25px', // Circular edges
+              },
+            }}
           />
-        </div>
-        <div className="form-group">
-        </div>
-        <button type="button" className="btn btn-primary" onClick={handleLogin}>Sign In</button>
-        <button type="button" className="btn btn-secondary" onClick={handleSignup}>Sign Up</button>
+          <a href="#" className="forgot-password">Forgot password?</a>
+        </div>  
+        <Button variant="contained" onClick={handleLogin}>Sign In</Button>
+        <Button variant="contained" onClick={handleSignup}>Sign Up</Button>
       </form>
-    </>
+    </div>
   );
 }
 
